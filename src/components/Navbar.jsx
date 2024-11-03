fbead114987ce08b96d977b6028eda64fc0bf69d
Rishabh Jain <107530140+rishabhjain208@users.noreply.github.com>
2024-11-03 12:10:08 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,13 +14,21 @@ const Navbar = ({ authToken, setAuthToken, email, setEmail }) => {
   };
 
   return (
-    <nav className="bg-[#212121] p-4">
+    <nav className="bg-[#212121] py-2 shadow-lg">
+      {" "}
+      {/* Reduced padding to py-2 */}
       <div className="container mx-auto flex items-center justify-between">
-        <div className="flex items-center ml-52 h-10 w-auto">
-          <img src={logo} alt="Logo" />
+        {/* Left placeholder to help center the logo */}
+        <div className="flex-1"></div>
+
+        {/* Centered logo with increased size and adjusted margin */}
+        <div className="flex-1 flex justify-center">
+          <img src={logo} alt="Logo" className="h-[100px] w-auto" />{" "}
+          {/* Set to h-16 for balance */}
         </div>
 
-        <div className="flex items-center space-x-4">
+        {/* Right section with login/signup or profile */}
+        <div className="flex-1 flex justify-end items-center space-x-4">
           {email ? (
             <div className="relative">
               <div
@@ -46,13 +54,13 @@ const Navbar = ({ authToken, setAuthToken, email, setEmail }) => {
             <>
               <Link
                 to="/login"
-                className="bg-white text-[#212121] font-medium border rounded-xl px-[10px] py-[5px]  transition duration-300"
+                className="bg-white text-[#212121] font-medium border rounded-full px-4 py-2 transition duration-300 hover:bg-gray-200"
               >
                 Login
               </Link>
               <Link
                 to="/signup"
-                className="text-[#ECECEC] font-medium px-[10px] py-[5px] border rounded-xl  transition duration-300"
+                className="text-[#ECECEC] font-medium px-4 py-2 border rounded-full border-[#ECECEC] transition duration-300 hover:bg-gray-700"
               >
                 Signup
               </Link>
